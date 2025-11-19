dbeb0c9092fc66ed03c661021c5f4491b5d8cce4
danieltesis232004 <danieltesis232004@gmail.com>
2025-11-19 07:58:42 -0500

    App

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 
+import CitaMedicaRoutes from './Routes/Citamedica.routes.js';
+import EspecialidadRoutes from './Routes/Especialidades.routes.js';
+import HistorialCRoutes from './Routes/HistorialC.js';
+import HorariosRoutes from './Routes/Horarios.routes.js';
+import MedicoRoutes from './Routes/Medico.routes.js';
+import RolesGRoutes from './Routes/RolesG.routes.js';
+import UsuarioRoutes from './Routes/Usuario.routes.js';
+
+
 const app = express()
 
 const corsOptions = {
@@ -12,6 +21,14 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 app.use(express.json())
+
+app.use('/api', CitaMedicaRoutes);
+app.use('/api', EspecialidadRoutes);
+app.use('/api', HistorialCRoutes);
+app.use('/api', HorariosRoutes);
+app.use('/api', MedicoRoutes);
+app.use('/api', RolesGRoutes);
+app.use('/api', UsuarioRoutes);
 
 
 
