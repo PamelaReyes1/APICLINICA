2257451a05475d124baf2ab4ae845a6811fd5348
danieltesis232004 <danieltesis232004@gmail.com>
2025-11-20 11:52:50 -0500

    Busqueda 2

--- src/Controladores/Especialidades.js
+++ src/Controladores/Especialidades.js
@@ -27,3 +27,23 @@ export const getEspecialidadPorId = async (req, res) => {
         res.status(500).json({ message: "Error al obtener especialidad", error });
     }
 };
+export const getEspecialidadPorIdMedico = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const [result] = await sql.query(
+            "SELECT * FROM ESPECIALIDADES WHERE ID_ESPECIALIDAD = ? ",
+            [id]
+        );
+
+        if (result.length > 0) {
+            res.json(result[0]);
+        } else {
+            res.status(404).json({ message: "Especialidad no encontrada" });
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: "Error al obtener especialidad", error });
+    }
+};
+
